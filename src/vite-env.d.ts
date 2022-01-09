be0a63c3460da95b3b460809121426e5af14f303
Megumu Niihara <niihara_megumu@libertyship.jp>
2022-01-09 10:56:55 +0900

    update--環境変数の型指定

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_APP_FIREBASE_DOMAIN: string;
+  readonly VITE_APP_FIREBASE_DATABASE: string;
+  readonly VITE_APP_FIREBASE_PROJECT_ID: string;
+  readonly VITE_APP_FIREBASE_PROJECT_STORAGE_BUCKET: string;
+  readonly VITE_APP_FIREBASE_MESSAGE_SENDER: string;
+  readonly VITE_APP_FIREBASE_APP_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
