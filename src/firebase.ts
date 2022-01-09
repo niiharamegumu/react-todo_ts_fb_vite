825ea4423badebc9c247e98a8e37c96c2d0b434a
Megumu Niihara <niihara_megumu@libertyship.jp>
2022-01-09 10:56:03 +0900

    update--環境変数の型指定

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_PROJECT_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGE_SENDER,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const fireStore = getFirestore(firebaseApp);
