c68350df7a8c73f7123ce85f680e9532f4cd917b
Megumu Niihara <nhrmgm1223@gmail.com>
2022-01-11 12:32:22 +0900

    update--providorの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
-import { Box, Flex, IconButton, Input, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  ChakraProvider,
+  Flex,
+  IconButton,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { AddIcon, SearchIcon } from "@chakra-ui/icons";
 
 import { VFC, useEffect, useState, ChangeEvent } from "react";
 import { useAllTasks } from "./hooks/useAllTasks";
 
 const App: VFC = () => {
-  const { getTasks, tasks } = useAllTasks();
+  // const { getTasks, tasks } = useAllTasks();
   const [input, setInput] = useState("");
 
   useEffect(() => {
-    getTasks();
+    // getTasks();
   }, []);
   const onChangeInputText = (e: ChangeEvent<HTMLInputElement>) =>
     setInput(e.target.value);
 
   return (
-    <Box p={30} textAlign="center">
-      <Stack w="50%" spacing={20} m="auto">
-        <Flex alignItems="center" justify="center" gap={10}>
-          <Input
-            value={input}
-            placeholder="New Tasks..."
-            p="1em"
-            onChange={onChangeInputText}
-            size="lg"
-          />
-          <IconButton aria-label="Search database" icon={<AddIcon />} />
-        </Flex>
-      </Stack>
-      {tasks.map((task) => (
+    <ChakraProvider>
+      <Box p={30} textAlign="center">
+        <Stack w="50%" spacing={20} m="auto">
+          <Flex alignItems="center" justify="center" gap={10}>
+            <Input
+              value={input}
+              placeholder="New Tasks..."
+              p="1em"
+              onChange={onChangeInputText}
+              size="lg"
+            />
+            <IconButton aria-label="Search database" icon={<AddIcon />} />
+          </Flex>
+        </Stack>
+        {/* {tasks.map((task) => (
         <p key={task.id}>{task.title}</p>
-      ))}
-    </Box>
+      ))} */}
+      </Box>
+    </ChakraProvider>
   );
 };
 
